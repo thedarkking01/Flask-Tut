c4478abc86921689b77ba12934206f717c6e33e4
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-08-29 23:04:26 +0530

    foundational of app

--- app.py
+++ app.py
@@ -4,13 +4,35 @@ from flask_scss import Scss
 
 # My App
 app = Flask(__name__)
-Scss(app, static_dir='static', asset_dir='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form['task']
+    new_task = Task(content=task_content)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
