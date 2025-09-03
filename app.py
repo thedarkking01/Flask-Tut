ff47fd52f1e19cc6a2199fb5fa25fc2f136e6d40
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-09-03 23:30:10 +0530

    improve ui of btn

--- app.py
+++ app.py
@@ -1,55 +1,3 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_scss import Scss
-
-# # My App
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-# class Task(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(200), nullable=False)
-
-# @app.route('/')
-# def index():
-#     tasks = Task.query.all()
-#     return render_template('index.html', tasks=tasks)
-
-# @app.route('/add', methods=['POST'])
-# def add_task():
-#     task_content = request.form['task']
-#     new_task = Task(content=task_content)
-#     db.session.add(new_task)
-#     db.session.commit()
-#     return redirect(url_for('index'))
-
-# @app.route('/delete/<int:id>', methods=['POST'])
-# def delete_task(id):
-#     task = Task.query.get_or_404(id)
-#     db.session.delete(task)
-#     db.session.commit()
-#     return redirect(url_for('index'))
-
-
-
-# @app.route('/edit/<int:id>', methods=['GET', 'POST'])
-# def edit_task(id):
-#     task = Task.query.get_or_404(id)
-
-#     if request.method == 'POST':
-#         task.content = request.form['task']
-#         db.session.commit()
-#         return redirect(url_for('index'))
-
-#     return render_template('edit.html', task=task)
-
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
@@ -65,7 +13,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-# Models (or import from models.py)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -87,7 +34,6 @@ class Task(db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -108,7 +54,6 @@ def register():
     return render_template('register.html', form=form)
 
 
-# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -125,7 +70,6 @@ def login():
     return render_template('login.html', form=form)
 
 
-# Logout
 @app.route('/logout')
 @login_required
 def logout():
